3d64c8f1bb1d1d1edcb33f8b7dfae4267b706f36
guoguangkai <guoguangkai@example.com>
2018-12-29 11:44:19 +0800

    新增 权限实体类

---
+++ src/main/java/com/kai/domain/PremissionEntity.java
@@ -0,0 +1,13 @@
+package com.kai.domain;
+
+import java.io.Serializable;
+/**
+ * 权限
+ * @author Administrator
+ *
+ */
+public class PremissionEntity implements Serializable {
+
+	private static final long serialVersionUID = 480249662914406218L;
+
+}
