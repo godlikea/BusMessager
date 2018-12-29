cae0c7a248e370df567e8757aae66254738b0403
guoguangkai <guoguangkai@example.com>
2018-12-29 11:38:11 +0800

    项目启动第一次测试

---
+++ src/main/java/com/kai/domain/AppEntity.java
@@ -0,0 +1,13 @@
+package com.kai.domain;
+
+import java.io.Serializable;
+/**
+ * 应用程序实体类
+ * @author Administrator
+ *
+ */
+public class AppEntity implements Serializable {
+
+	private static final long serialVersionUID = -5975433066937503174L;
+
+}
